8c0ce2421432ef24d58b629cf479adbc4e8a8374
Arthur12035 <arthur.beignon1203@gmail.com>
2024-05-15 09:08:52 +0200

    import algo

---
+++ dev_algo.py
@@ -0,0 +1,18 @@
+from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
+
+
+liste_produit = []
+def demande_produit():
+    for n in range(i) :
+        liste_produit[i] = input("qu'elle est le produit n°", i,"que vous souhaitez prendre ? ")
+
+
+
+
+i = 0
+nb_boucle = 0
+i = input("Combien de produit devaient aller vous cherchez dans la boutique ? ")
+nb_boucle = i 
+i = i + 1
+#if (i>0 & i<100000):
+    #while (nb_boucle < i):
