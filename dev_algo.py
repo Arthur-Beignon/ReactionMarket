224e9af15132fb24af368a80f17dbbc22eaabe6f
Arthur12035 <arthur.beignon1203@gmail.com>
2024-05-15 15:17:31 +0200

    ajout de : ouverture json et demande de fichier, liste produits

--- dev_algo.py
+++ dev_algo.py
@@ -1,6 +1,5 @@
-from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel
 from filepile import *
-
+import json, os
 
 liste_produit = []
 liste_placement = []
@@ -8,21 +7,22 @@ nb_produits = 0
 
 #Permet de demander au client combien de produits doit-il aller chercher dans le magasin
 def dmd_nb_produit(): 
-    
-    condition_while = False
-    while condition_while == False : 
-        nb_produits = int(input("Combien de produits devez-vous aller chercher dans le magasin ReactionMarket ? \n"))
-        if nb_produits < 9999999 and nb_produits > 0:
-            condition_while = True
-        else:
-            print("Le nombre de produits doit être compris entre 1 et 9999999.")
-    return nb_produits
+    while True: 
+        try:
+            nb_produits = int(input("Combien de produits devez-vous aller chercher dans le magasin ReactionMarket ? \n"))
+            if nb_produits > 0:
+                return nb_produits
+            else:
+                print("Le nombre de produits doit être supérieur à zéro.")
+        except ValueError:
+            print("Veuillez entrer un nombre valide.\n")
 
 
 #Permet de demander au client quelles sont les produits qu'il a besoin dans le magasin
-def demande_produit():
-    for n in range(nb_produits) :
-        liste_produit[n] = input("qu'elle est le produit n°","que vous souhaitez prendre ?")
+def demande_produit(nb_produits):
+    for n in range(nb_produits):
+        produit = input(f"Quel est le produit n°{n+1} que vous souhaitez prendre ? ")
+        liste_produit.append(produit)
     return liste_produit
 
 
@@ -51,7 +51,7 @@ A chaque clé est associée la distance qui sépare ce sommet du sommet_depart.
 
         for voisin in voisins :
 
-            if voisin not in bk_liste :                                          # ligne optionnelle
+            if voisin not in bk_liste :                                   
 
                 nouvelle_distance: int = distance[sommet][0] + voisins[voisin]
                 
@@ -62,7 +62,48 @@ A chaque clé est associée la distance qui sépare ce sommet du sommet_depart.
     return distance
 
 
+def demander_nom_fichier():
+    while True:
+        nom_fichier = input("Quel est le nom du fichier que vous voulez lire ? \n")
+        if nom_fichier.strip() :  
+            if os.path.isfile(nom_fichier) :  
+                return nom_fichier
+            else:
+                print("Le fichier spécifié n'existe pas. Veuillez réessayer.\n")
+        else:
+            print("Le nom du fichier ne peut pas être vide. Veuillez réessayer.\n")
 
 
 
 
+def lire_fichier(nom_fichier) :
+    with open(nom_fichier) as f:
+        data = json.load(f)
+        print("Légumes : ",data['Légumes'])
+        print("Poissons : ", data['Poissons'])
+        print("Viandes : ", data['Viandes'])
+        print("Épicerie : ", data['Épicerie'])
+        print("Épicerie sucrée : ", data['Épicerie sucrée'])
+        print("Petit déjeuner : ", data['Petit déjeuner'])
+        print("Fruits : ", data['Fruits'])
+        print("Rayon frais : ", data['Rayon frais'])
+        print("Crèmerie : ", data['Crèmerie'])
+        print("Conserves : ", data['Conserves'])
+        print("Apéritifs : ", data['Apéritifs'])
+        print("Boissons : ", data['Boissons'])
+        print("Articles Maison : ", data['Articles Maison'])
+        print("Hygiène : ", data['Hygiène'])
+        print("Bureau : ", data['Bureau'])
+        print("Animaux : ", data['Animaux'])
+
+
+
+def main():
+    nb_produits = dmd_nb_produit()
+    nom_fichier = demander_nom_fichier()
+    lire_fichier(nom_fichier)
+    demande_produit(nb_produits)
+
+if __name__ == "__main__":
+    main()
+nb_produits < 9999999
